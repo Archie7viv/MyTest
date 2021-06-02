1a0f5163afa330541295862b0e897467b48110ef
Archie7viv <popoff7viv@gmail.com>
2021-06-02 18:58:02 +0300

    Logger added

---
+++ Infrastructure/FileLogger.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Infrastructure
+{
+    public class FileLogger : ILogger
+    {
+        private readonly object fileLock = new object();
+        private readonly string datetimeFormat;
+        private string logFilename;
+        string pathPrefix = "../../";
+
+        public FileLogger(string logFilename)
+        {
+            datetimeFormat = "yyyy-MM-dd HH:mm:ss.fff";
+            this.logFilename = $"{logFilename}.log";
+
+            // Log file header line
+            string logHeader = logFilename + " is created.";
+            if (!File.Exists(logFilename))
+            {
+                WriteLine(DateTime.Now.ToString(datetimeFormat) + " " + logHeader);
+            }
+        }
+
+        public void Error(string text)
+        {
+            WriteFormattedLog(LogLevel.ERROR, text);
+        }
+
+        public void Info(string text)
+        {
+            WriteFormattedLog(LogLevel.INFO, text);
+        }
+
+        public void Warning(string text)
+        {
+            WriteFormattedLog(LogLevel.WARNING, text);
+        }
+
+        private void WriteLine(string text, bool append = false)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(text))
+                {
+                    return;
+                }
+                lock (fileLock)
+                {
+                    using (StreamWriter writer = new StreamWriter(pathPrefix + logFilename, append, Encoding.UTF8))
+                    {
+                        writer.WriteLine(text);
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private void WriteFormattedLog(LogLevel level, string text)
+        {
+            string pretext;
+            switch (level)
+            {
+                case LogLevel.INFO:
+                    pretext = DateTime.Now.ToString(datetimeFormat) + " [INFO]    ";
+                    break;
+                case LogLevel.WARNING:
+                    pretext = DateTime.Now.ToString(datetimeFormat) + " [WARNING] ";
+                    break;
+                case LogLevel.ERROR:
+                    pretext = DateTime.Now.ToString(datetimeFormat) + " [ERROR]   ";
+                    break;
+                default:
+                    pretext = "";
+                    break;
+            }
+
+            WriteLine(pretext + text, true);
+        }
+
+        [Flags]
+        private enum LogLevel
+        {
+            INFO,
+            WARNING,
+            ERROR
+        }
+    }
+}
---
+++ Infrastructure/ILogger.cs
@@ -0,0 +1,11 @@
+﻿namespace Infrastructure
+{
+    public interface ILogger
+    {
+        void Error(string text);
+
+        void Info(string text);
+
+        void Warning(string text);
+    }
+}
